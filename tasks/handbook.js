15541170bc787990818937d8759f27d67802e917
Craig Cook <boycook@Craigs-Air.home>
2015-11-19 16:16:17 +0000

    Updated to write config file

--- tasks/handbook.js
+++ tasks/handbook.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  function getTiddlers(url, html, target, done) {
+  function getTiddlers(url, html, target, configFile, done) {
     var tiddlers;
     var options = {
       url: url,
@@ -29,6 +29,7 @@ module.exports = function(grunt) {
     function callback(error, response, body) {
       if (!error && response.statusCode === 200) {
         var tiddlers = JSON.parse(body);
+        var config = [];
         for (var i = 0, len = tiddlers.length; i < len; i++) {
             var tiddler = tiddlers[i];
             var json = {
@@ -36,17 +37,22 @@ module.exports = function(grunt) {
               "extends": ["base.json"],
               "targetPath" : "index.html",
               "partials%add": [],
-              "depth": "../",
+              "depth": "../../",
               "title": tiddler.title,
-              "content": tiddler.render
+              "content": tiddler.render,
+              "tags": tiddler.tags
             };
-
+            
+            var item = {};
+            item['gen/handbook/' + tiddler.title + '/*.html'] = 'templates/html/handbook/' + tiddler.title + '/*.html';
+            config.push(item);
             var text = JSON.stringify(json);
             var path = target + '/' + tiddler.title;
             fs.mkdirSync(path);
             fs.writeFileSync(path + '/index.json', text);
             fs.copySync(html, path + '/index.html');
             if (i === len-1) {
+              fs.writeFileSync(configFile, JSON.stringify(config));
               done();
             }
         }        
@@ -58,7 +64,6 @@ module.exports = function(grunt) {
 
     request(options, callback);
   }
-
 
   grunt.registerTask('sac2mHandbook', 'Generating the Scaling Agile Handbook.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
@@ -72,11 +77,12 @@ module.exports = function(grunt) {
     console.log('Using source URL: ' + options.url);
     console.log('Using HTML tempate: ' + options.template.html);
     console.log('Using target: ' + options.target);
+    console.log('Using config file: ' + options.configFile);
 
     if (!fs.existsSync(options.target)) {
       fs.mkdirSync(options.target);
     }
 
-    getTiddlers(options.url, options.template.html, options.target, done);
+    getTiddlers(options.url, options.template.html, options.target, options.configFile, done);
   });
 };
